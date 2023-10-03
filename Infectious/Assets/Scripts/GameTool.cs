07e3927023789c0c98d6aa4c819678cce36c84c5
daruni238 <daruni238@gmail.com>
2023-10-03 18:31:57 +0800

    small fixes

--- Infectious/Assets/Scripts/GameTool.cs
+++ Infectious/Assets/Scripts/GameTool.cs
@@ -4,12 +4,14 @@ using UnityEditor;
 public class GameTool : EditorWindow
 {
     gamemanager GameManager;
+    npcScript npcScript;
     int Score;
     int Day;
     GameObject NPC;
     GameObject ActiveNPC;
     int CurrentNPC;
     int NPCsThisDay;
+    int dieaseType;
 
 
     [MenuItem("Tools/GameTool")]
@@ -29,9 +31,14 @@ public class GameTool : EditorWindow
         ActiveNPC = GameManager.ActiveNPC;
         NPCsThisDay = GameManager.NPCsThisDay;
 
+        npcScript = GameObject.FindGameObjectWithTag("NPC").GetComponent<npcScript>();
+        dieaseType = npcScript.diseaseType; 
+
         Score = EditorGUILayout.IntField("Current score", Score); //Currently not in use, score system still in development
         Day = EditorGUILayout.IntField("Move to Day", Day); //Currently not in use, day system still in development
         CurrentNPC = EditorGUILayout.IntField("Current NPC", CurrentNPC);
+        dieaseType = EditorGUILayout.IntField("dieaseType", dieaseType);
+
 
         if (GUILayout.Button("Next NPC"))
         {
