5ea4e7da1ed79cfc003473b3a4789c04029bb3bb
Fitz Rosli <fitri1101@hotmail.com>
2023-10-03 17:10:51 +0800

    UI Update
    - Finish Main Menu UI
    - Create Settings UI in Main Menu
    - Added SceneManager script

---
+++ Infectious/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public void startGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainLevel");
+    }
+
+
+
+}
