ee2077432e71e2ffeff376dc46d44ba7b6720ed7
Aylwin-Chin <0133437@student.uow.edu.my>
2023-10-10 02:21:13 +0800

    Revert "Urine Test Result"
    This reverts commit d42263531e8fc8801d7e0427d8eb9d0bf981b4b0.

--- Infectious/Assets/Scripts/TestScripts.cs
+++ Infectious/Assets/Scripts/TestScripts.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
 public class TestScripts : MonoBehaviour
 {
@@ -17,9 +16,6 @@ public class TestScripts : MonoBehaviour
     public TMP_Text testTitle;
     public TMP_Text testResultText;
     public GameObject Buttons;
-    public bool AssetReady;
-    public GameObject UrineTestTrue;
-    public GameObject UrineTestFalse;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,8 +33,6 @@ public class TestScripts : MonoBehaviour
         Buttons.SetActive(false);
         TestPromt.SetActive(true);
         testTitle.text = testName;
-        
-        if(AssetReady==false){
         if(activeNPC.GetComponent<npcScript>().patientSymptomList[symptomToCheck]==true){
             testResultText.text = testResultTextTrue;
         }
@@ -46,24 +40,8 @@ public class TestScripts : MonoBehaviour
             testResultText.text = testResultTextFalse;
         }
         Debug.Log("Symptom " + symptomToCheck + "" + activeNPC.GetComponent<npcScript>().patientSymptomList[symptomToCheck]);
-        }
-        else{
-        if(activeNPC.GetComponent<npcScript>().patientSymptomList[symptomToCheck]==true){
-            testResultText.text = "";
-            UrineTestTrue.SetActive(true);
-        }
-        else{
-            testResultText.text = "";
-            UrineTestFalse.SetActive(true);
-        }
-    }
     }
     void NextNPCSpawned(){
         activeNPC = gameManager.GetComponent<gamemanager>().ActiveNPC;
-    }
-
-    void closemenu(){
-        UrineTestTrue.SetActive(false);
-        UrineTestFalse.SetActive(false);
     }
 }
