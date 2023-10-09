92fb4dc7103928b841aa1d9e6ec22406d2b0deac
Aylwin-Chin <0133437@student.uow.edu.my>
2023-10-09 09:25:57 +0800

    Test Promt Popup (couldn't sleep irl :( so I did work)

--- Infectious/Assets/Scripts/TestScripts.cs
+++ Infectious/Assets/Scripts/TestScripts.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TestScripts : MonoBehaviour
@@ -8,6 +9,13 @@ public class TestScripts : MonoBehaviour
     public int symptomToCheck;
     private GameObject activeNPC;
     public GameObject gameManager;
+    public GameObject TestPromt;
+    public string testName;
+    public string testResultTextTrue;
+    public string testResultTextFalse;
+    public TMP_Text testTitle;
+    public TMP_Text testResultText;
+    public GameObject Buttons;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +29,16 @@ public class TestScripts : MonoBehaviour
     }
     void OnMouseUpAsButton()
     {
+        transform.parent.gameObject.SetActive(false);
+        Buttons.SetActive(false);
+        TestPromt.SetActive(true);
+        testTitle.text = testName;
+        if(activeNPC.GetComponent<npcScript>().patientSymptomList[symptomToCheck]==true){
+            testResultText.text = testResultTextTrue;
+        }
+        else{
+            testResultText.text = testResultTextFalse;
+        }
         Debug.Log("Symptom " + symptomToCheck + "" + activeNPC.GetComponent<npcScript>().patientSymptomList[symptomToCheck]);
     }
     void NextNPCSpawned(){
