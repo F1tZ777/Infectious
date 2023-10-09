08402b7b6bd7873fe41220454ae69c88a96e998c
Fitz Rosli <fitri1101@hotmail.com>
2023-10-09 21:25:20 +0800

    Animation Start

---
+++ Infectious/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public SceneManager SceneManager;
+    public Color normalColor, hoverColor, pressedColor;
+    private SpriteRenderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.color = hoverColor;
+    }
+
+    private void OnMouseDown()
+    {
+        rend.color = pressedColor;
+        Debug.Log("Pressed");
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Released");
+            rend.color = normalColor;
+            SceneManager.startGame();
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        rend.color = normalColor;
+    }
+}
