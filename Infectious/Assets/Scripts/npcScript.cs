e9cbacff8ce7a1fa00118d8cdae5303a19cc72a8
Aylwin-Chin <0133437@student.uow.edu.my>
2023-10-10 01:45:09 +0800

    NPC Looks Initialization
    dsfahioudfhjlfadsuiodsfauijadfujioedfauio

--- Infectious/Assets/Scripts/npcScript.cs
+++ Infectious/Assets/Scripts/npcScript.cs
@@ -13,7 +13,12 @@ public class npcScript : MonoBehaviour
     [SerializeField]private float []OddsForSymptomsDehydration = new float[10];
     [HideInInspector]public bool []patientSymptomList = new bool[10];
     [SerializeField]private int []patientDiseaseList;
-
+    [SerializeField]private GameObject []head;
+    [SerializeField]private GameObject []eyes;
+    [SerializeField]private GameObject []nose;
+    [SerializeField]private GameObject []mouth;
+    [SerializeField]private GameObject []body;
+    private int gender;
     
     //[SerializeField]private float OddsForSymptoms;
     // Start is called before the first frame update
@@ -61,7 +66,62 @@ public class npcScript : MonoBehaviour
             else{
                 patientSymptomList[i]=false;
             }
-            Debug.Log("Symptom"+i+" "+patientSymptomList[i]);
+            //Debug.Log("Symptom"+i+" "+patientSymptomList[i]);
+        }
+        RandomizerNPCAppearance();
+    }
+
+    private void RandomizerNPCAppearance(){
+        gender = Random.Range(0,2);
+        for(int i=0; i<head.Length; i++){
+            head[i].SetActive(false);
+        }
+        for(int i=0; i<eyes.Length; i++){
+            eyes[i].SetActive(false);
+        }
+        for(int i=0; i<nose.Length; i++){
+            nose[i].SetActive(false);
+        }
+        for(int i=0; i<mouth.Length; i++){
+            mouth[i].SetActive(false);
+        }
+        for(int i=0; i<body.Length; i++){
+            body[i].SetActive(false);
+        }
+
+        
+        if(gender==0){
+            head[Random.Range(0,2)].SetActive(true);
+        }
+        else{
+            head[2].SetActive(true);
+        }
+
+       
+        if(patientSymptomList[6]==false){
+            eyes[Random.Range(0,5)].SetActive(true);
+        }
+        else{
+            eyes[Random.Range(5,10)].SetActive(true);
+        }
+
+        
+        if(gender==0){
+            nose[Random.Range(0,2)].SetActive(true);
+        }
+        else{
+            nose[Random.Range(2,4)].SetActive(true);
+        }
+        
+
+        mouth[Random.Range(0,5)].SetActive(true);
+
+
+        if(gender==0){
+            body[0].SetActive(true);
+        }
+        else{
+            body[1].SetActive(true);
         }
     }
 }
